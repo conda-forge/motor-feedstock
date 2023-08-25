b40f98c09e52003a92a11ea5c4fc6e5ee14ba8cf
Steven Silvester <steven.silvester@ieee.org>
2023-08-24 20:58:12 -0500

    test updates

--- recipe/run_the_tests.sh
+++ recipe/run_the_tests.sh
@@ -5,12 +5,12 @@ unset SSL_CERT_FILE
 
 export DB_PATH="$SRC_DIR/temp-mongo-db"
 export LOG_PATH="$SRC_DIR/mongolog"
-export DB_PORT=27272
+export DB_PORT=27017
 export PID_FILE_PATH="$SRC_DIR/mongopidfile"
 
 mkdir "$DB_PATH"
 
-mongod --dbpath="$DB_PATH" --fork --logpath="$LOG_PATH" --port="$DB_PORT" --pidfilepath="$PID_FILE_PATH"
+mongod --dbpath="$DB_PATH" --fork --logpath="$LOG_PATH" --port="$DB_PORT" --pidfilepath="$PID_FILE_PATH" --setParameter="enableTestCommands=1"
 
 python -m pytest -v
 
